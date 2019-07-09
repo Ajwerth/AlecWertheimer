c8b44d2ed8c1f2f7b01787709679897eca9f9de4
Alec Wertheimer <alec@wintreetz.com>
2019-07-09 08:05:00 -0400

    added logo and changed main theme  color

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: '#ff971b',
       marginBottom: `1.45rem`,
     }}
   >
